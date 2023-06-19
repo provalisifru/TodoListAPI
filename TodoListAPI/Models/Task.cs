74969fe79c21032f7aeefed569c0236f48ecbe32
unknown <nemanja.stojanov21@gmail.com>
2023-06-19 14:27:53 +0200

    Added date and time for tasks with repeatabillity functionallity

--- TodoListAPI/Models/Task.cs
+++ TodoListAPI/Models/Task.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TodoList.Models;
-public partial class Task {
-    public Guid TaskId { get; set; }
+namespace TodoList.Models
+{
+    public partial class Task
+    {
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; } = null!;
+        public string TaskCategory { get; set; } = null!;
+        public string TaskDescription { get; set; } = null!;
+        public sbyte IsCompleted { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime TaskDate { get; set; }
 
-    public string TaskName { get; set; } = null!;
+        public sbyte IsRepeatable { get; set; }
 
-    public string TaskCategory { get; set; } = null!;
-
-    public string TaskDescription { get; set; } = null!;
-
-    public sbyte IsCompleted { get; set; }
-
-    public Guid UserId { get; set; }
-
+        public string RepetitionFrequency { get; set; } = null!;
+    }
 }
